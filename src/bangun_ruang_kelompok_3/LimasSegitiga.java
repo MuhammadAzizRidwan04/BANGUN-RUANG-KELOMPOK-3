0e9624fd67418b3b5a09c6e6a4af4c78358c56a1
Alfianaziz <muhammadalfiannuraziz9@gmail.com>
2024-09-28 17:15:03 +0700

    limassegitigaalfianaziz

---
+++ src/bangun_ruang_kelompok_3/LimasSegitiga.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bangun_ruang_kelompok_3;
+
+/**
+ *
+ * @author Santri IT
+ */
+public class LimasSegitiga {
+
+    double alas, tingi_alas, luas_sisi_tegak, tingi_limas_segitiga;
+
+    public void setAlas(double alas) {
+        this.alas = alas;
+    }
+
+    public double getAlas() {
+        return alas;
+    }
+
+    public void setTinggiAlas(double tinggi_alas) {
+        this.tingi_alas = tinggi_alas;
+    }
+
+    public double getTinggiAlas() {
+        return tingi_alas;
+    }
+
+    public void setLuasSisiTegak(double luas_sisi_tegak) {
+        this.luas_sisi_tegak = luas_sisi_tegak;
+    }
+
+    public double getLuasSisiTegak() {
+        return luas_sisi_tegak;
+    }
+
+    public void setTinggiLimasSegitiga(double tinggi_limas_segitiga) {
+        this.tingi_limas_segitiga = tinggi_limas_segitiga;
+    }
+
+    public double getTinggiLimasSegitiga() {
+        return tingi_limas_segitiga;
+    }
+
+    public double getLuasPermukaan() {
+        return (1.0 / 2.0 * alas * tingi_alas) + (3 * luas_sisi_tegak);
+    }
+
+    public double getVolume() {
+        return 1.0 / 3.0 * (1.0 / 2.0 * alas * tingi_alas) * tingi_limas_segitiga;
+    }
+}
