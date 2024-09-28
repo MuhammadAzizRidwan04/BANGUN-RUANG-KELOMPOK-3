ed507e759f4df93508b901b63cca9c631f70ba0d
MuhammadAzizRidwan04 <akungithub088@gmail.com>
2024-09-28 16:36:06 +0700

    updatepunyayusuf

--- src/bangun_ruang_kelompok_3/MainClass.java
+++ src/bangun_ruang_kelompok_3/MainClass.java
@@ -12,6 +12,24 @@ public class MainClass {
     
     public static void main(String[] args) {
         
+        PrismaSegitiga ps = new PrismaSegitiga();
+        ps.setAlas(3.8);
+        ps.setTinggi_alas(3.7);
+        ps.setKeliling_alas(4.1);
+        ps.setTinggi_prisma(2.3);
+        System.out.println("\n ____________________________________________");
+        System.out.println("| Muhammad Alfian nur aziz : Prisma Segitiga |");
+        System.out.println("|____________________________________________|");
+        System.out.println("\nAlas \t\t\t: " + ps.getAlas());
+        System.out.println("Tinggi alas \t\t: "+ps.getTinggi_alas());
+        System.out.println("Kelilinhg alas \t\t: "+ps.getKeliling_alas());
+        System.out.println("Tingi prisma \t\t: "+ps.getTinggi_prisma());
+        System.out.println("Rumus Luas Permukaan \t: (2 * 1/2 * Alas * Tingi alas) + (Keliling alas * Tingi alas)");
+        System.out.println("Rumus Volume \t\t: (1/2 * Alas * Tinggi alas) * Tingi prisma");
+        System.out.println("Hasil Luas Permukaan \t: " + ps.getLuaspermukaan());
+        System.out.println("Hasil Volume \t\t: " + ps.getvolume());
+        System.out.println("=================================================================================");
+        
         Balok blk = new Balok();
         blk.setPanjang(2.8);
         blk.setLebar(4.7);
@@ -26,6 +44,18 @@ public class MainClass {
         System.out.println("Rumus Volume \t\t: Panjang * Lebar * Tinggi ");
         System.out.println("Hasil Luas Permukaan \t: " + blk.getLuasPermukaan());
         System.out.println("Hasil Volume \t\t: " + blk.getVolume());
+        System.out.println("=================================================================================");
+        
+        Kubus kbs = new Kubus();
+        kbs.setSisi(3.7);
+        System.out.println("\n _____________________________________");
+        System.out.println("| M yusuf bahtiar nur saputra : Kubus |");
+        System.out.println("|_____________________________________|");
+        System.out.println("\nSisi \t\t\t: " + kbs.getSisi());
+        System.out.println("Rumus Luas Permukaan \t: 6 * Sisi^2 ");
+        System.out.println("Rumus Volume \t\t: sisi^3");
+        System.out.println("Hasil Luas Permukaan \t: " + kbs.getLuasPermukaan());
+        System.out.println("Hasil Volume \t\t: " + kbs.getVolume());
         System.out.println("=================================================================================");
     }
     
