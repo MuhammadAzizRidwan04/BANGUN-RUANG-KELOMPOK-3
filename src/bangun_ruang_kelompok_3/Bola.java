2684e409097a95e39fe65bb83012cffb3f3eda34
anisaurdatunnafiah <anisaurdatunnafiah@gmail.com>
2024-10-14 14:13:02 +0700

    updateanisa

--- src/bangun_ruang_kelompok_3/Bola.java
+++ src/bangun_ruang_kelompok_3/Bola.java
@@ -11,7 +11,7 @@ package bangun_ruang_kelompok_3;
 public class Bola extends Bangun_ruang {
 
     double luas, volume;
-
+//
     public void setluaspermukaan() {
         super.luas = 4 * Math.PI * Math.pow(jari_jari, 2);
     }
